a61fe93e8ea59aa3a487fa12608344bd9ddca580
Eldon B. Tenorio <ebtenorio@yahoo.com>
2016-07-21 08:42:06 +0800

    SCC Endpoint Decryptor
    SCC Endpoint Decryptor

---
+++ Decryptor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Decryptor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Decryptor")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b953e4c-04ba-4923-b8da-fa4170bdb4c1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
---
+++ SccEndpointDecryptor/EndpointDecryptor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+// USAGE
+// Author   : Eldon B. Tenorio
+// Date     : October 13, 2015
+// 
+// 1. Import the latest SccEndpointDecryptor.dll to the Reference.
+// 2. Import or Using it in your code               : Using SccEndpointDecryptor;
+// 3. Declare a variable of type EndPointDecryptor  : EndpointDecryptor objEndpointDecr = new EndpointDecryptor();
+// 4. Set the path of the endpoint.bin              : objEndpointDecr.SourceFilname = @"C:\ProgramData\4sIT\SCC\.SCC\endpoints.bin";
+// 5. Extract the decrypted file content            : string fileContent = objEndpointDecr.ExtractDecrytedContent();
+
+namespace SccEndpointDecryptor
+{
+    public class EndpointDecryptor
+    {
+        const string SECRET_DECR_KEY = "EJKDODDD";
+        const string SECRET_INIT_VEC = "AOEMEYDD";
+        private string sourceFilename = "";
+
+        public string SourceFilename
+        {
+            get { return sourceFilename; }
+            set { this.sourceFilename = value; }
+        }
+
+        public EndpointDecryptor()
+        {
+        }
+
+        public string ExtractDecrytedContent()
+        {
+            string tmpDecryptedContent = "";
+
+            try
+            {
+                if (this.sourceFilename.Trim() != "")
+                {
+                    if (File.Exists(this.sourceFilename))
+                    {
+                        DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+                        DES.Key = ASCIIEncoding.ASCII.GetBytes(SECRET_DECR_KEY);
+                        DES.IV = ASCIIEncoding.ASCII.GetBytes(SECRET_INIT_VEC);
+                        FileStream fsread = new FileStream(this.sourceFilename, FileMode.Open, FileAccess.Read);
+                        ICryptoTransform desdecrypt = DES.CreateDecryptor();
+                        CryptoStream cryptostreamDecr = new CryptoStream(fsread, desdecrypt, CryptoStreamMode.Read);
+                        tmpDecryptedContent = new StreamReader(cryptostreamDecr).ReadToEnd();
+                    }
+                    else
+                    {
+                        tmpDecryptedContent = "File path incorrect or does not exist.";
+                    }
+                }
+                else
+                {
+                    tmpDecryptedContent = "Source filename not specified.";
+                }
+            }
+            catch (Exception ex)
+            {
+                tmpDecryptedContent = ex.Message;
+            }
+            return tmpDecryptedContent;
+        }
+    }
+}
